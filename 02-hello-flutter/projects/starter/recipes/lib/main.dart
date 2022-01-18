eeecb5a4b208fb86b0f24dfcabd0b75f2000d220
Jatin Nagar <itsjatinnagar@gmail.com>
2022-01-18 21:12:33 +0530

    Building a recipe list - Done

--- 02-hello-flutter/projects/starter/recipes/lib/main.dart
+++ 02-hello-flutter/projects/starter/recipes/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipes/recipe.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -38,7 +39,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: Container(),
+        child: ListView.builder(
+          itemCount: Recipe.samples.length,
+          itemBuilder: (BuildContext context, int index) {
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(height: 14.0),
+            Text(
+              recipe.label,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w700,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
