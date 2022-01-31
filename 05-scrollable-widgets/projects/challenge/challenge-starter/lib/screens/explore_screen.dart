30769a116ca7041be7ee3f4df3f8c5d6b0346376
Jatin Nagar <itsjatinnagar@gmail.com>
2022-01-31 19:47:39 +0530

    Challenges - Done

--- 05-scrollable-widgets/projects/challenge/challenge-starter/lib/screens/explore_screen.dart
+++ 05-scrollable-widgets/projects/challenge/challenge-starter/lib/screens/explore_screen.dart
@@ -4,10 +4,43 @@ import '../api/mock_fooderlich_service.dart';
 import '../components/components.dart';
 import '../models/models.dart';
 
-class ExploreScreen extends StatelessWidget {
-  final mockService = MockFooderlichService();
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
 
-  ExploreScreen({Key? key}) : super(key: key);
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
+  final mockService = MockFooderlichService();
+  late ScrollController _scrollController;
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('i am at the bottom!');
+    }
+
+    if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('i am at the top!');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +49,7 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
           return ListView(
+            controller: _scrollController,
             scrollDirection: Axis.vertical,
             children: [
               TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
