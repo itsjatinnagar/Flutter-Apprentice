52ef4223b94969a4e62b95a16bd69d0da4ac76bf
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-04 13:32:01 +0530

    Managing tab state - Done

--- 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/empty_grocery_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../models/models.dart';
 
 class EmptyGroceryScreen extends StatelessWidget {
   const EmptyGroceryScreen({Key? key}) : super(key: key);
@@ -34,7 +37,9 @@ class EmptyGroceryScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(30.0),
               ),
               color: Colors.green,
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<TabManager>(context, listen: false).goToRecipes();
+              },
             ),
           ],
         ),
