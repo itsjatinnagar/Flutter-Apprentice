5959d9d29967d1367b4fd327c078305e2843755a
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-06 19:21:03 +0530

    Building GroceryTile - Done

--- 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
+++ 06-interactive-widgets/projects/starter/lib/components/grocery_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../models/grocery_item.dart';
 
@@ -20,6 +22,52 @@ class GroceryTile extends StatelessWidget {
     return Container(
       height: 100.0,
       color: Colors.red,
+    );
+  }
+
+  Widget buildImportance() {
+    if (item.importance == Importance.low) {
+      return Text(
+        'Low',
+        style: GoogleFonts.lato(
+          decoration: textDecoration,
+        ),
+      );
+    } else if (item.importance == Importance.medium) {
+      return Text(
+        'Medium',
+        style: GoogleFonts.lato(
+          fontWeight: FontWeight.w800,
+          decoration: textDecoration,
+        ),
+      );
+    } else if (item.importance == Importance.high) {
+      return Text(
+        'High',
+        style: GoogleFonts.lato(
+          color: Colors.red,
+          fontWeight: FontWeight.w900,
+          decoration: textDecoration,
+        ),
+      );
+    } else {
+      throw Exception('This importance type does not exist');
+    }
+  }
+
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
+
+  Widget buildCheckbox() {
+    return Checkbox(
+      value: item.isComplete,
+      onChanged: onComplete,
     );
   }
 }
