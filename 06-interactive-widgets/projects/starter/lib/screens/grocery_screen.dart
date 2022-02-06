6e88eb8a9d8ddaa7ce78701497b1988d00907edd
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-06 15:45:34 +0530

    Creating the screen to add grocery items - Done

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_screen.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 
 import '../models/models.dart';
 import 'empty_grocery_screen.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
@@ -12,7 +13,21 @@ class GroceryScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          final manager = Provider.of<GroceryManager>(context, listen: false);
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => GroceryItemScreen(
+                onCreate: (item) {
+                  manager.addItem(item);
+                  Navigator.pop(context);
+                },
+                onUpdate: (item) {},
+              ),
+            ),
+          );
+        },
       ),
       body: buildGroceryScreen(),
     );
