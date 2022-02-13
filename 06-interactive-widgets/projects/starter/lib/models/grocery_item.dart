648a4b8008f0be527b1a32bb8be858389990ed75
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-13 20:34:48 +0530

    fix bug: isComplete not changing

--- 06-interactive-widgets/projects/starter/lib/models/grocery_item.dart
+++ 06-interactive-widgets/projects/starter/lib/models/grocery_item.dart
@@ -41,6 +41,7 @@ class GroceryItem {
       color: color ?? this.color,
       quantity: quantity ?? this.quantity,
       date: date ?? this.date,
+      isComplete: isComplete ?? this.isComplete,
     );
   }
 }
