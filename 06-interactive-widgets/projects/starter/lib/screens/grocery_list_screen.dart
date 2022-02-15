1729239465014d623aeac270a01665b3d666946e
Jatin Nagar <itsjatinnagar@gmail.com>
2022-02-15 20:57:22 +0530

    Adding gestures - Done

--- 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_list_screen.dart
@@ -20,13 +20,30 @@ class GroceryListScreen extends StatelessWidget {
         itemCount: groceryItems.length,
         itemBuilder: (context, index) {
           final item = groceryItems[index];
-          return GroceryTile(
-            key: Key(item.id),
-            item: item,
-            onComplete: (change) {
-              if (change != null) {
-                manager.completeItem(index, change);
-              }
+          return InkWell(
+            child: GroceryTile(
+              key: Key(item.id),
+              item: item,
+              onComplete: (change) {
+                if (change != null) {
+                  manager.completeItem(index, change);
+                }
+              },
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GroceryItemScreen(
+                    originalItem: item,
+                    onUpdate: (item) {
+                      manager.updateItem(item, index);
+                      Navigator.pop(context);
+                    },
+                    onCreate: (_) {},
+                  ),
+                ),
+              );
             },
           );
         },
